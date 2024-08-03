68a6952ca3b06fb4914c8d6d1076241cd788913e
Palm Dreamer Agency <admin@palmdreamer.com>
2024-08-04 09:02:33 +1000

    Checkpoint

--- src/app/components/tiers.jsx
+++ src/app/components/tiers.jsx
@@ -47,7 +47,7 @@ const tiers = [
   },
 ];
 
-export default function Example() {
+export default function Tiers() {
   return (
     <div className="bg-white py-24 sm:py-32">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
