a3ad45dae138808666c0ceb120916e0f295c9595
Palm Dreamer Agency <admin@palmdreamer.com>
2024-08-05 08:17:02 +1000

    Checkpoint

--- src/app/components/navigation.jsx
+++ src/app/components/navigation.jsx
@@ -21,8 +21,12 @@ import {
   MenuItem,
   MenuItems,
 } from "@headlessui/react";
-import { MagnifyingGlassIcon } from "@heroicons/react/20/solid";
-import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
+import {
+  MagnifyingGlassIcon,
+  PhoneIcon,
+  EnvelopeIcon,
+} from "@heroicons/react/20/solid";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 
 import logo from "../../../public/dd-logo.jpg";
 
@@ -113,39 +117,17 @@ export default function Navigation() {
               className="relative flex-shrink-0 rounded-full bg-white p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
             >
               <span className="absolute -inset-1.5" />
-              <span className="sr-only">View notifications</span>
-              <BellIcon aria-hidden="true" className="h-6 w-6" />
-            </button>
 
-            {/* Profile dropdown */}
-            <Menu as="div" className="relative ml-4 flex-shrink-0">
-              <div>
-                <MenuButton className="relative flex rounded-full bg-white focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
-                  <span className="absolute -inset-1.5" />
-                  <span className="sr-only">Open user menu</span>
-                  <img
-                    alt=""
-                    src={user.imageUrl}
-                    className="h-8 w-8 rounded-full"
-                  />
-                </MenuButton>
-              </div>
-              <MenuItems
-                transition
-                className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
-              >
-                {userNavigation.map((item) => (
-                  <MenuItem key={item.name}>
-                    <a
-                      href={item.href}
-                      className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100"
-                    >
-                      {item.name}
-                    </a>
-                  </MenuItem>
-                ))}
-              </MenuItems>
-            </Menu>
+              <PhoneIcon aria-hidden="true" className="h-6 w-6" />
+            </button>
+            <button
+              type="button"
+              className="relative flex-shrink-0 rounded-full bg-white p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+            >
+              <span className="absolute -inset-1.5" />
+
+              <EnvelopeIcon aria-hidden="true" className="h-6 w-6" />
+            </button>
           </div>
         </div>
         <nav
@@ -193,28 +175,21 @@ export default function Navigation() {
         </div>
         <div className="border-t border-gray-200 pb-3 pt-4">
           <div className="flex items-center px-4">
-            <div className="flex-shrink-0">
-              <img
-                alt=""
-                src={user.imageUrl}
-                className="h-10 w-10 rounded-full"
-              />
-            </div>
-            <div className="ml-3">
-              <div className="text-base font-medium text-gray-800">
-                {user.name}
-              </div>
-              <div className="text-sm font-medium text-gray-500">
-                {user.email}
-              </div>
-            </div>
             <button
               type="button"
               className="relative ml-auto flex-shrink-0 rounded-full bg-white p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
             >
               <span className="absolute -inset-1.5" />
-              <span className="sr-only">View notifications</span>
-              <BellIcon aria-hidden="true" className="h-6 w-6" />
+
+              <PhoneIcon aria-hidden="true" className="h-6 w-6" />
+            </button>
+            <button
+              type="button"
+              className="relative ml-auto flex-shrink-0 rounded-full bg-white p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+            >
+              <span className="absolute -inset-1.5" />
+
+              <EnvelopeIcon aria-hidden="true" className="h-6 w-6" />
             </button>
           </div>
           <div className="mt-3 space-y-1 px-2">
