dc350dfc07dc644a8e9b1b1576f1e15886a8ff14
Palm Dreamer Agency <admin@palmdreamer.com>
2024-08-05 08:20:35 +1000

    Add Footer

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 
 import Navigation from "./components/navigation";
+import Footer from "./components/footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,6 +17,7 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
         <Navigation />
         {children}
+        <Footer />
       </body>
     </html>
   );
