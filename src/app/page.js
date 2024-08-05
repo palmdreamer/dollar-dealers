3822318f248285d07219f39337dda60f6d68e484
Palm Dreamer Agency <admin@palmdreamer.com>
2024-08-05 14:27:01 +1000

    Checkpoint

--- src/app/page.js
+++ src/app/page.js
@@ -2,10 +2,18 @@ import Image from "next/image";
 
 import Tiers from "./components/tiers";
 
+import Navigation from "./components/navigation";
+import Footer from "./components/footer";
+import { Fragment } from "react";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Tiers />
-    </main>
+    <Fragment>
+      <Navigation />
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <Tiers />
+      </main>
+      <Footer />
+    </Fragment>
   );
 }
