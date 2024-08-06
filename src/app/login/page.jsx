c3b6bc1bc1c5dcdc864ae71fd2cdd0541e86adb8
Palm Dreamer Agency <admin@palmdreamer.com>
2024-08-06 14:11:00 +1000

    Show Sign In/ Admin On Condition

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,11 +1,28 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import app from "@/lib/firebase";
 import Admin from "../components/admin";
 import SignIn from "../components/sign-in";
 
+const auth = getAuth(app);
+
 export default function Login() {
-  return (
-    <>
-      <Admin />
-      <SignIn />
-    </>
-  );
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      if (currentUser) {
+        setUser(currentUser);
+      } else {
+        setUser(null);
+      }
+    });
+
+    // Cleanup subscription on unmount
+    return () => unsubscribe();
+  }, []);
+
+  return <>{user ? <Admin /> : <SignIn />}</>;
 }
